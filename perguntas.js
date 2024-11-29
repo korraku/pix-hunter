25623288ebfac5ac9f318d40a38c9daa00c54e76
korraku <matheus.tymniak03@gmail.com>
2024-11-29 09:54:40 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,51 @@
+criaCartao(
+    'Camuflagem',
+    'O que é o Mecanismo da Camuflagem?',
+    'É uma adaptação evolutiva que permite a organismo se esconderem de predadores, misturando-se com o ambiente.'
+)
+
+criaCartao(
+    'Camuflagem',
+    'O que é Criptismo?',
+    'É a camuflagem em que o organismo se parece com o ambiente ao seu redor.'
+)
+
+criaCartao(
+    'Camuflagem',
+    'O que é Mimetismo?',
+    'É quando um organismo imita a aparência de outro para se proteger.'
+)
+
+    
+criaCartao(
+    'Camuflagem',
+    'O que é Mimetismo de Objetos?',
+    'É quando organismo se camuflam parecendo objetos inanimados do ambiente, como folhas, galhos ou pedras.'
+)
+criaCartao(
+    'Camuflagem',
+    'O que é Camuflagem Disruptiva?',
+    'É o tipo de camuflagem que envolve padrões de cores constratantes que quebram a silhueta do organismo, tornando difícil para os predadores ou presas discernirem sua forma exata.'
+)
+criaCartao(
+    'Camuflagem',
+    'Ilusão Óptica?',
+    'Alguns animais utilizam ilusão óptica para confundir predadores, que tem espinhos longos que criam uma ilusão de tamanho maior e mais ameaçador do que realmente é.'
+)
+criaCartao(
+    'Camuflagem',
+    'O que é Camuflagem Comportamental?',
+    'É quando além das características físicas que ajudam na camuflagem, alguns animais ajustam seu comportamento para maximizar a eficácia da camuflagem.'
+)
+criaCartao(
+    'Camuflagem',
+    'Cite exemplos de Camuflagem Comportamental?',
+    'Se mover lentamente, ficar imóvel por longos períodos ou se posicionar em ambientes onde sua camuflagem será mais eficaz.'
+)
+criaCartao(
+    'Camuflagem',
+    'Cite exemplo da Importância Evolutiva?',
+    'A camuflagem é um exemplo clássico de seleção natural, onde os organismos que melhor se misturam com o ambiente têm uma maior chance de sobreviver.'
+)
+
+
